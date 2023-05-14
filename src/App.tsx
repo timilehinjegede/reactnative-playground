9a3f8c47cf03cd15490ce71f15310ae0239d0778
Timilehin Jegede <timilehintimothy.jegede@gmail.com>
2023-05-14 13:03:03 +0100

    chore: setup navigation for forecast club module.

--- src/App.tsx
+++ src/App.tsx
@@ -7,94 +7,34 @@
 
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './HomeScreen';
+import ForecastClubScreen from './forecastClub/ForecastClubScreen';
 
-type BoxProps = {title: string};
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
-  const ModuleBoxItem = (boxProps: BoxProps) => {
-    return (
-      <View style={styles.moduleBox}>
-        <Text style={styles.moduleBoxText}>{boxProps.title}</Text>
-      </View>
-    );
-  };
-
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-      }}>
-      <View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            paddingHorizontal: 15,
-            paddingBottom: 10,
-          }}>
-          <Text style={styles.welcomeText}>
-            <Text>{'Hello,\n'}</Text>
-            <Text
-              style={{
-                color: 'black',
-                fontWeight: '600',
-                fontSize: 20,
-              }}>
-              Welcome to my playground!
-            </Text>
-          </Text>
-
-          <View style={styles.avatar}>
-            <Text>TJ</Text>
-          </View>
-        </View>
-        <View style={styles.rowStyle}>
-          <ModuleBoxItem title={'Forecast\nClub'} />
-          <ModuleBoxItem title={'Dukandar\nShopping'} />
-        </View>
-        <View style={styles.rowStyle}>
-          <ModuleBoxItem title={'Travel\nService'} />
-          <ModuleBoxItem title={'Jersey\nKit'} />
-        </View>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ForecastClub"
+          component={ForecastClubScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  moduleBox: {
-    backgroundColor: 'grey',
-    height: 100,
-    flex: 1,
-    borderRadius: 8,
-    justifyContent: 'center',
-  },
-  moduleBoxText: {
-    textAlign: 'center',
-    fontSize: 18,
-    color: 'white',
-    fontWeight: '600',
-  },
-  rowStyle: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    gap: 20,
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-  },
-  welcomeText: {
-    fontSize: 16,
-    lineHeight: 30,
-  },
-  avatar: {
-    height: 45,
-    width: 45,
-    borderRadius: 25,
-    backgroundColor: 'grey',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default App;
---
+++ src/HomeScreen.tsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+} from 'react-native';
+import {
+  NavigationAction,
+  NavigationContainer,
+  NavigationProp,
+} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+type BoxProps = {title: string; onPress: () => void};
+
+const HomeScreen = ({navigation}: any) => {
+  const ModuleBoxItem = (boxProps: BoxProps) => {
+    return (
+      <TouchableOpacity style={styles.moduleBox} onPress={boxProps.onPress}>
+        <View>
+          <Text style={styles.moduleBoxText}>{boxProps.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}>
+      <View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            paddingHorizontal: 15,
+            paddingBottom: 10,
+          }}>
+          <Text style={styles.welcomeText}>
+            <Text>{'Hello,\n'}</Text>
+            <Text
+              style={{
+                color: 'black',
+                fontWeight: '600',
+                fontSize: 20,
+              }}>
+              Welcome to my playground!
+            </Text>
+          </Text>
+
+          <View style={styles.avatar}>
+            <Text>TJ</Text>
+          </View>
+        </View>
+        <View style={styles.rowStyle}>
+          <ModuleBoxItem
+            title={'Forecast\nClub'}
+            onPress={() => navigation.navigate('ForecastClub')}
+          />
+          <ModuleBoxItem
+            title={'Dukandar\nShopping'}
+            onPress={() => navigation.navigate('')}
+          />
+        </View>
+        <View style={styles.rowStyle}>
+          <ModuleBoxItem
+            title={'Travel\nService'}
+            onPress={() => navigation.navigate('')}
+          />
+          <ModuleBoxItem
+            title={'Jersey\nKit'}
+            onPress={() => navigation.navigate('')}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  moduleBox: {
+    backgroundColor: 'grey',
+    height: 100,
+    flex: 1,
+    borderRadius: 8,
+    justifyContent: 'center',
+  },
+  moduleBoxText: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: 'white',
+    fontWeight: '600',
+  },
+  rowStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  welcomeText: {
+    fontSize: 16,
+    lineHeight: 30,
+  },
+  avatar: {
+    height: 45,
+    width: 45,
+    borderRadius: 25,
+    backgroundColor: 'grey',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default HomeScreen;
