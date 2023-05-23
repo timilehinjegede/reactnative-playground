d671a2de332cb4845132b798b2a9ded9dabb9b2b
Timilehin Jegede <timilehintimothy.jegede@gmail.com>
2023-05-23 22:28:54 +0100

    chore: setup drawer navigation.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugin: [
+    'react-native-reanimated/plugin',
+    {
+      relativeSourceLocation: true,
+    },
+  ],
 };
--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {PropsWithChildren, ReactElement} from 'react';
 import {
   View,
   Text,
   SafeAreaView,
   StyleSheet,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
+import {Icon} from 'react-native-vector-icons/Icon';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 type BoxProps = {title: string; onPress: () => void};
 
@@ -21,56 +24,102 @@ const HomeScreen = ({navigation}: any) => {
   };
 
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-      }}>
-      <View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            paddingHorizontal: 15,
-            paddingBottom: 10,
-          }}>
-          <Text style={styles.welcomeText}>
-            <Text>{'Hello,\n'}</Text>
-            <Text
-              style={{
-                color: 'black',
-                fontWeight: '600',
-                fontSize: 20,
-              }}>
-              Welcome to my playground!
+    <SafeAreaView style={{flex: 1}}>
+      <ScrollView
+        style={{
+          flex: 1,
+        }}>
+        <View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              paddingHorizontal: 15,
+              paddingBottom: 10,
+            }}>
+            <Text style={styles.helloText}>
+              <Text>{'Hello,\n'}</Text>
+              <Text style={styles.welcomeText}>Welcome to my playground!</Text>
             </Text>
+
+            <View style={styles.avatar}>
+              <Text>TJ</Text>
+            </View>
+          </View>
+
+          <Text
+            style={[
+              styles.welcomeText,
+              styles.paddingHorizontal,
+              styles.paddingTop,
+            ]}>
+            UI Designs Replication
+          </Text>
+          <View style={styles.rowStyle}>
+            <ModuleBoxItem
+              title={'Forecast\nClub'}
+              onPress={() => navigation.navigate('ForecastClub')}
+            />
+            <ModuleBoxItem
+              title={'Dukandar\nShopping'}
+              onPress={() => navigation.navigate('')}
+            />
+          </View>
+          <View style={styles.rowStyle}>
+            <ModuleBoxItem
+              title={'Travel\nService'}
+              onPress={() => navigation.navigate('')}
+            />
+            <ModuleBoxItem
+              title={'Jersey\nKit'}
+              onPress={() => navigation.navigate('JerseyKit')}
+            />
+          </View>
+          <Text
+            style={[
+              styles.welcomeText,
+              styles.paddingHorizontal,
+              styles.paddingTop,
+            ]}>
+            React Navigation
           </Text>
 
-          <View style={styles.avatar}>
-            <Text>TJ</Text>
+          <View style={styles.rowStyle}>
+            <ModuleBoxItem
+              title={'Native\nStack'}
+              onPress={() => navigation.navigate('ForecastClub')}
+            />
+            <ModuleBoxItem
+              title={'Drawers'}
+              onPress={() => navigation.navigate('')}
+            />
+          </View>
+          <View style={styles.rowStyle}>
+            <ModuleBoxItem
+              title={'Bottom Tabs'}
+              onPress={() => navigation.navigate('ForecastClub')}
+            />
+            <ModuleBoxItem
+              title={'Top Tabs'}
+              onPress={() => navigation.navigate('')}
+            />
+          </View>
+
+          <Text
+            style={[
+              styles.welcomeText,
+              styles.paddingHorizontal,
+              styles.paddingTop,
+            ]}>
+            Misc
+          </Text>
+          <View style={styles.rowStyle}>
+            <ModuleBoxItem title={''} onPress={() => undefined} />
+            <ModuleBoxItem title={''} onPress={() => undefined} />
           </View>
         </View>
-        <View style={styles.rowStyle}>
-          <ModuleBoxItem
-            title={'Forecast\nClub'}
-            onPress={() => navigation.navigate('ForecastClub')}
-          />
-          <ModuleBoxItem
-            title={'Dukandar\nShopping'}
-            onPress={() => navigation.navigate('')}
-          />
-        </View>
-        <View style={styles.rowStyle}>
-          <ModuleBoxItem
-            title={'Travel\nService'}
-            onPress={() => navigation.navigate('')}
-          />
-          <ModuleBoxItem
-            title={'Jersey\nKit'}
-            onPress={() => navigation.navigate('JerseyKit')}
-          />
-        </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -96,9 +145,20 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     paddingVertical: 10,
   },
-  welcomeText: {
+  helloText: {
     fontSize: 16,
     lineHeight: 30,
+  },
+  welcomeText: {
+    color: 'black',
+    fontWeight: '600',
+    fontSize: 20,
+  },
+  paddingHorizontal: {
+    paddingHorizontal: 15,
+  },
+  paddingTop: {
+    paddingTop: 15,
   },
   avatar: {
     height: 45,
